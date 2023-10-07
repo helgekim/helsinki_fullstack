9b96743fb3711ecc256d4882101899764ebd5861
Kim Oleg <kim.oleg@outlook.kr>
2023-10-07 18:54:59 +0500

    Exercise 1.4: refactoring. All parts are stored as objects in a especially defined array.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -52,23 +52,25 @@ function Total({total}) {
 
 const App = () => {
   const course = 'Half Stack application development'
-    const part1 = {
+  const parts = [
+    {
       name: 'Fundamentals of React',
       exercises: 10
-    }
-    const part2 = {
+    },
+    {
       name: 'Using props to pass data',
       exercises: 7
-    }
-    const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
     }
+  ]
 
   return (
     <div>
       <Header CourseName={course}/>
-      <Content parts={[part1, part2, part3]}/>
+      <Content parts={parts}/>
     </div>
   )
 }
