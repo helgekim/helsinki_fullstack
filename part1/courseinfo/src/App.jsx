49f43a8ff07ddb884bbc3e4f1b96d8ba44492ce6
Kim Oleg <kim.oleg@outlook.kr>
2023-10-07 19:03:45 +0500

    Exercise 1.4: refactoring. Course data are stored as a single object. It is then passed onto the Course component which then renders all the data using Header, Part and Total components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,31 +2,26 @@ import { useState } from 'react';
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Header({CourseName}) {
+function Header({courseName}) {
   return (
     <div>
-      <h1> {CourseName} </h1>
+      <h1> {courseName} </h1>
     </div>
   )
 }
 
-function Content({parts})
+function Course({info})
 {
-  let partsdata = parts.map(part => {
-    /*
-    console.log("Array: ", exercises)
-    console.log("Array position", num)
-    console.log("Array value", exercises[num])
-    */
+  let partsdata = info.parts.map(part => {
     return(
       <Part part={part.name} exercises={part.exercises}/>
     )
   })
 
-  let total = parts.map(element => element.exercises).reduce((accumulator, currentValue) => accumulator + currentValue)
-  console.log(partsdata)
+  let total = info.parts.map(element => element.exercises).reduce((accumulator, currentValue) => accumulator + currentValue)
   return (
     <div>
+      <Header courseName = {info.name}/>
       {partsdata}
       <Total total={total}/>
     </div>
@@ -51,26 +46,28 @@ function Total({total}) {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header CourseName={course}/>
-      <Content parts={parts}/>
+      <Course info={course}/>
     </div>
   )
 }
