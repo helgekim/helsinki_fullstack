d7c53f112d892b78bb0633bf8f863dbb5172c158
Kim Oleg <kim.oleg@outlook.kr>
2023-10-08 22:51:27 +0500

    Exercise 1.6: unicafe app created. Can give your opinion and those opinions are shown

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGoodOpinions = () => setGood(good + 1);
+  const increaseNeutralOpinions = () => setNeutral(neutral + 1);
+  const increaseBadOpinions = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <div>
+          <h1> UniCafe Reviews! </h1>
+      </div>
+      <div>
+          <h2> Give us a review! </h2>
+      </div>
+      <div>
+        <button onClick={increaseGoodOpinions}>good</button>
+        <button onClick={increaseNeutralOpinions}>neutral</button>
+        <button onClick={increaseBadOpinions}>bad</button>
+      </div>
+      <div>
+        <div>
+          <h2> What do people think of us? </h2>
+        </div>
+        <div>
+          <p> Good: {good} </p>
+          <p> Neutral: {neutral} </p>
+          <p> Bad: {bad} </p>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default App
