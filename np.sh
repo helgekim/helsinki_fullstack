86f45957282e67c98dbd49e14d1f1b33a40212c9
Kim Oleg <kim.oleg@outlook.kr>
2023-10-09 21:34:29 +0500

    np.sh creates a new vite project asking what i want. yes, i am lazy. it also installs and runs that project for the first time. presented by me to you in bash. also anecdotes created.

---
+++ np.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+
+echo "Where would you like to create a project?"
+read place
+
+echo "What would you like the name to be?"
+read name
+
+echo "Vite it is. What template?"
+read template
+
+cd $place
+npm create vite@latest $name --  --template $template
+cd $name
+npm install
+
+echo "Now we are going to launch it"
+npm run dev
+
+
+
