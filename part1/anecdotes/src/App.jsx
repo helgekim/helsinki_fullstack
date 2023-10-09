99af24b8a12de16d8a9e879b448fbe0323174719
Kim Oleg <kim.oleg@outlook.kr>
2023-10-09 22:37:46 +0500

    exercise 1.13: can vote now!

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -21,10 +21,28 @@ function Button ({text, onclick}) {
 const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
 
+  function anecdotevotes () {
+    let pretable = [];
+    for (let a = 0; a < anecdotes.length; a++) {
+      pretable.push([a, 0]);
+    }
+    return Object.fromEntries(pretable)
+  }
+
+  const [votes, setVotes] = useState(anecdotevotes());
+
+  function voteForJoke() {
+      const newVotes = {
+        ...votes,
+      }
+      newVotes[selected] += 1;
+      return setVotes(newVotes)
+  }
 
   function nextJoke() {
     return (selected + 1) >= anecdotes.length ? setSelected(0) : setSelected(selected + 1)
   }
+
   function randomJoke() {
     function newNumber() {
       return Math.floor(Math.random() * anecdotes.length);
@@ -32,11 +50,14 @@ const App = ({anecdotes}) => {
     return setSelected(newNumber());
   }
 
+
   return (
     <div>
      <p>  {anecdotes[selected]} </p>
+     <p> this joke has {votes[selected]} votes </p>
      <Button text={"next"} onclick = {nextJoke}/>
      <Button text={"random"} onclick = {randomJoke}/>
+     <Button text={"vote"} onclick={voteForJoke}/>
     </div>
   )
 }
