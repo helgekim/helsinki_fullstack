e8f31ce7fd0f8eea298282551e963f4b48895aa3
Kim Oleg <kim.oleg@outlook.kr>
2023-10-13 09:37:23 +0500

    part 2, ex 2.3: can generate an infinite amount of courses

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 function Header({courseName}) {
   return (
     <div>
-      <h1> {courseName} </h1>
+      <h2> {courseName} </h2>
     </div>
   )
 }
@@ -47,30 +47,65 @@ function Course({course})
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      }
-    ],
-  }
+function Courses({courses}) {
+  return courses.map(course => <Course key={course.id} course={course}/>)
+}
 
-  return <Course course={course} />
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
+
+  return (
+    <div>
+    <div>
+      <h1> Web development cirriculum </h1>
+    </div>
+
+    <Courses courses={courses} />
+
+    </div>)
 }
 
 export default App
