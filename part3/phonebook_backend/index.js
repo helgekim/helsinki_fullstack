10abb16c87f8a1da6918e5de43ac5aed0eeff25e
Kim Oleg <kim.oleg@outlook.kr>
2023-11-28 21:33:29 +0500

    3.22: lint

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app  = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const Contact = require('./modules/Note');
+const Contact = require("./modules/Note");
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 let contacts = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{ 
+		"id": 1,
+		"name": "Arto Hellas", 
+		"number": "040-123456"
+	},
+	{ 
+		"id": 2,
+		"name": "Ada Lovelace", 
+		"number": "39-44-5323523"
+	},
+	{ 
+		"id": 3,
+		"name": "Dan Abramov", 
+		"number": "12-43-234345"
+	},
+	{ 
+		"id": 4,
+		"name": "Mary Poppendieck", 
+		"number": "39-23-6423122"
+	}
 ]; 
 
 app.use(express.json());
@@ -39,43 +39,43 @@ app.use(express.json());
 
 
 const logger = require("morgan");
-app.use(logger(':method :url :status - :content'));
-logger.token('content', function (req, res) { return JSON.stringify(req.body) })
+app.use(logger(":method :url :status - :content"));
+logger.token("content", function (req, res) { return JSON.stringify(req.body); });
 
 
-app.get('/', (request, response) => {
- response.send(`<div><h1>Welcome to phonebook's API!</h1><p>send all requests to <i>api/persons/....</i></p></div>`)
-})
+app.get("/", (request, response) => {
+	response.send("<div><h1>Welcome to phonebook's API!</h1><p>send all requests to <i>api/persons/....</i></p></div>");
+});
 
 
-app.get('/info', (request, response) => {
-let contactLength = contacts.length;
-let date = new Date();
-response.send(`<div>
+app.get("/info", (request, response) => {
+	let contactLength = contacts.length;
+	let date = new Date();
+	response.send(`<div>
 	<div>
 	<p> This phonebook has ${contactLength} entries </p>
 	</div>
 	<div>
 	Today is ${date}
 	</div>
-</div`)
-})
+</div`);
+});
 
-app.get('/api/persons', (request, response) =>  {
- Contact.find({}).then(resources => response.send(resources)).catch(error => {
- console.log("Couldn't find no damn data", error);
- response.send(contacts)
-})
-})
+app.get("/api/persons", (request, response) =>  {
+	Contact.find({}).then(resources => response.send(resources)).catch(error => {
+		console.log("Couldn't find no damn data", error);
+		response.send(contacts);
+	});
+});
 
-app.get('/api/persons/:id', (request, response) => {
- Contact.find({_id: request.params.id}).then(resource => resource ? response.json(resource) : response.status(404).end()).catch(error => {
-	console.log(error);
-	response.status(500).end();
-	})
-})
+app.get("/api/persons/:id", (request, response) => {
+	Contact.find({_id: request.params.id}).then(resource => resource ? response.json(resource) : response.status(404).end()).catch(error => {
+		console.log(error);
+		response.status(500).end();
+	});
+});
 
-app.post('/api/persons', (
+app.post("/api/persons", (
 	request, response
 ) => 
 
@@ -86,35 +86,38 @@ app.post('/api/persons', (
 
 	if (!contact.name) {
 		// console.log("No name")
-		return response.status(403).end()
+		return response.status(403).end();
 	} else if (!contact.number) {
 		// console.log("No number")
-		return response.status(403).end()
+		return response.status(403).end();
 	}
 
 	
 
 	let newContact = new Contact({
-	"name": contact.name,
-	"number": contact.number});
+		"name": contact.name,
+		"number": contact.number});
 
 	// contacts.push(newContact)
 	newContact.save().then(contact => response.json(request.body)).catch(
-	error => console.log(error)
-	)
+		error => {
+	 console.log(error);
+	 response.status(400).json(error).end();
+		}
+	);
 }
 
-)
+);
 
-app.put('/api/persons/:id', (request, response) => {
+app.put("/api/persons/:id", (request, response) => {
 
-	console.log(request.params.id)
+	console.log(request.params.id);
 
-	Contact.findByIdAndUpdate(request.params.id, request.body, {new: true, runValidators: true, context:'query'}).then(contact => contact ? response.json(contact) : response.status(300).end()).catch(error => {
-	console.log(error); response.status(500).end()
-	})
+	Contact.findByIdAndUpdate(request.params.id, request.body, {new: true, runValidators: true, context:"query"}).then(contact => contact ? response.json(contact) : response.status(300).end()).catch(error => {
+		console.log(error); response.status(500).end();
+	});
 
-})
+});
 
 /*
 app.get('/api/persons/:id', (request, response) => {	
@@ -130,11 +133,11 @@ app.get('/api/persons/:id', (request, response) => {
  	Contact.find({_id: id}).then(resource => response.send(resource)).catch(error => console.log("Failed to find a resource"))
 })*/
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
 	let id = request.params.id;
-        // contacts = contacts.filter(contact => contact.id != Number(id));
-	Contact.deleteOne({_id: id}).then(resource => response.status(204).end()).catch(error => console.log("Failed to delete a contact", error))
-})
+	// contacts = contacts.filter(contact => contact.id != Number(id));
+	Contact.deleteOne({_id: id}).then(resource => response.status(204).end()).catch(error => console.log("Failed to delete a contact", error));
+});
 
-const PORT = process.env.PORT ||  3001
-app.listen(PORT, () => {console.log(`Server runs on port ${PORT}`)});
+const PORT = process.env.PORT ||  3001;
+app.listen(PORT, () => {console.log(`Server runs on port ${PORT}`);});
--- part3/phonebook_backend/modules/Note.js
+++ part3/phonebook_backend/modules/Note.js
@@ -1,28 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL).then(
- response => console.log("Connected")
+	response => console.log("Connected")
 ).catch(
- error => console.log(error.message)
+	error => console.log(error.message)
 );
 
 const contactSchema = new mongoose.Schema({
-  name: 
-	{type: String,
-	minLength: 3,
-	required: true},
-  number: String
-})
+	name: {type: String,
+		minLength: 3,
+		required: true},
+	number: {
+		type: String,
+		validate: {
+	 validator: function (v) {
+	  return /^\d{2,3}-\d{7,8}/.test(v);
+			}
+		}
+	}
+});
 
 contactSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-}})
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	}});
 
-module.exports = mongoose.model("Contact", contactSchema)
+module.exports = mongoose.model("Contact", contactSchema);
