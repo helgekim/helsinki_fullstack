dfa1d4d9ee2287c0dbe53fa0912520712c52c0a6
Kim Oleg <kim.oleg@outlook.kr>
2023-12-02 17:21:35 +0500

    4.4: helper functions and unit tests, step2

--- part3/bloglist/utils/list_helper.js
+++ part3/bloglist/utils/list_helper.js
@@ -2,6 +2,9 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => blogs.length == 1 ? blogs[0].likes : blogs.map(element => element.likes).reduce((a, b) => a + b, 0)
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
