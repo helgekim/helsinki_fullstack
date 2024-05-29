d6a72ae4cacaf09521e252b2e6b700cf05ade8af
KYT <kim.oleg@outlook.kr>
2024-05-29 23:36:56 +0500

    4.20-21: doesn't work yet but token auth implemented for deletion and posts

--- part3/bloglist/controllers/Blogs.js
+++ part3/bloglist/controllers/Blogs.js
@@ -29,20 +29,25 @@ Blogs.get('/', async (request, response) => {
 })
 
 Blogs.post('/', (request, response) => {
-	const authorization_header = request.rawHeaders[9]
-	const auth_head_ver = (authorization_header.split(" "))[1]
+	const auth_head_ver  = request.header('Authorization').split(' ')[1]
 	// console.log(authorization_header)
 	// console.log(auth_head_ver);
 	let user_data = {};	
 
+	// console.log(authorization_header)
+	// console.log(auth_head_ver)
+
 	jwt.verify(auth_head_ver, process.env.SECRET, (err, decoded) => {
+
 	if (err) {
 	return response.status(402).json({"error": "unable to authorize"})
 	} 
+
 	user_data = decoded;
 	})
 
 	// console.log(user_data)
+
 
 	if (request.body) {
 	// console.log(request)
@@ -54,8 +59,48 @@ Blogs.post('/', (request, response) => {
 	})
 	newBlog.save().then(resource => response.status(301).json(resource).end()).catch(data => {error(data); response.status(300).json({"error": `${data}`}).end()})
 	} else {
-	 response.status(300).end();
+	 return response.status(300).end();
 	}
+})
+
+Blogs.delete('/:id', async (request, response) => {
+
+// doesn't work yet
+	const blogToDelete = request.params.id;
+/*	
+}
+*/
+	// console.log(user_data)
+
+//	console.log(request.params.id)
+	const this_blog = await Blog.findOne({_id: blogToDelete})
+//	console.log(this_blog)
+	
+
+	const auth_head_ver = request.header('Authorization').split(" ")[1]
+	// console.log(auth_head_ver);
+	let user_data = {};	
+
+	jwt.verify(auth_head_ver, process.env.SECRET, (err, decoded) => {
+	if (err) {
+	return response.status(402).json({"error": "unable to authorize"})
+	} 
+	user_data = decoded;
+	})
+
+//	console.log(user_data)
+//	console.log(this_blog.author.toString())
+//	console.log(user_data.id)
+
+	if (this_blog.author.toString() ==  user_data.id) {
+//	console.log("We are here")
+
+	await Blog.deleteOne({_id: request.param.id})
+	return response.status(203).status({"situation": "completed"})
+	}
+		
+// response.json({"status": "function in development"})
+
 })
 
 module.exports = Blogs;
